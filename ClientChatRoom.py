de5c977ceed00863b2f5803e26101322ff6a3633
julius <juliusniyonzima423@gmail.com>
2025-02-03 21:57:26 -0600

    added client chat and  server chat

--- ClientChatRoom.py
+++ ClientChatRoom.py
@@ -2,15 +2,31 @@
 import socket, threading
 #constants
 DEST_IP = socket.gethostbyname(socket.gethostname())
-DEST_PORT = 4521
+DEST_PORT = 35412
 BYTE_SIZE = 2048
 
 #create socket
 client_socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((DEST_PORT,DEST_IP)) #connected as tuple with dest port and ipp
+client_socket.connect((DEST_IP, DEST_PORT)) #connected as tuple with dest port and ipp
 def send_message():
     """The client sends the message to the server"""
     pass
 def receive_message():
     """Receive message from the server"""
-    pass
+    while True:
+        """use try and except to receive the message from server"""
+        try:
+            message = client_socket.recv(BYTE_SIZE).decode("utf-8")
+            #check for flag name
+            if message == "NAME":
+                name = input("Enter name: ")
+                client_socket.send(name.encode('utf-8'))
+            else:
+                print(message)
+        except :
+            #error
+            print("an error occured")
+            client_socket.close()
+            break
+#start chart
+receive_message()
--- ServerChatRoom.py
+++ ServerChatRoom.py
@@ -19,9 +19,28 @@ CLIENT_NAME = []
 def broadcast_message(message):
     """send messages to all connected clients"""
     pass
+
 def receive_message(client_socket):
     """receive incoming call"""
     pass
 def connect_client():
     """Connect incoming clients"""
-    pass
+    #infinite listening
+    while True:
+        #accept incoming connections
+        client_socket, address = server_socket.accept()
+        print(f"Connected with{address} ..")
+        #get the name of the client getting connected
+        client_socket.send("NAME".encode("utf-8"))
+        client_name = client_socket.recv(SIZE_OF_BYTES).decode('utf-8')
+        #append to list
+        CLIENT_NAME.append(client_name)
+        CLIENT_SOCKET_LIST.append(client_socket)
+        #update the server, and all clients
+        print(f"Name of new client is {client_name}\n")
+        client_socket.send(f"{client_name} you connected to server".encode('utf-8'))
+        broadcast_message(f"{client_name} joined the server".encode("utf-8"))
+
+#connect client
+connect_client()
+
